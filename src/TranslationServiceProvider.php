d34f7d38272bce80c84844f996b5b46bff1b891a
Bro Qiang <broqiang@qq.com>
2017-09-19 01:16:45 +0800

    测试通过

--- src/TranslationServiceProvider.php
+++ src/TranslationServiceProvider.php
@@ -11,46 +11,15 @@ use BroQiang\LaravelLang\Commands\Publish as PublishCommand;
 
 class TranslationServiceProvider extends LaravelTranslationServiceProvider
 {
-    /**
-     * @var bool
-     */
-    protected $inLumen = false;
 
     /**
      * Register the service provider.
      */
     public function register()
     {
-        if ($this->app instanceof \Laravel\Lumen\Application) {
-            $this->inLumen = true;
-
-            $this->app->configure('app');
-
-            unset($this->app->availableBindings['translator']);
-        }
-
         parent::register();
 
         $this->registerCommands();
-    }
-
-    /**
-     * Register the translation line loader.
-     */
-    protected function registerLoader()
-    {
-        $this->app->singleton('translation.loader', function ($app) {
-            $paths = [
-                base_path('vendor/broqiang/laravel-lang/src/'),
-            ];
-
-            if ($this->inLumen) {
-                $this->app['path.lang'] = base_path('vendor/laravel/lumen-framework/resources/lang');
-                array_push($paths, base_path('resources/lang/'));
-            }
-
-            return new FileLoader($app['files'], $app['path.lang'], $paths);
-        });
     }
 
     /**
